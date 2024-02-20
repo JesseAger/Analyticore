01713372a55c4d2dcec50fbe0ebba63ccbd7f468
JesseAger <agerjesse6@gmail.com>
2024-02-20 17:49:54 +0300

    analyticore

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-//import {RiMenu3line, RiCloseLine} from 'react-icons/ri';
+
+import React, { useState } from 'react';
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css';
 // import logo from '../../assets/logo-removebg.png';
 
-
-const navbar = () => {
+const Menu = () => (
+ <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#about">About Us</a></p>
+  <p><a href="#possibility">Portfolio</a></p>
+  <p><a href="#features">Contact</a></p>
+  <p><a href="#blog">Resources</a></p>
+  </>
+)
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className='dpr__navbar'>
       <div className='dpr__navbar-links'>
@@ -12,16 +22,35 @@ const navbar = () => {
             {/* <img src={logo} alt="logo" /> */}
           </div>
           <div className='dpr__navbar-links_container'>
-            <p><a href="#home">Home</a></p>
-            <p><a href="#about">About Us</a></p>
-            <p><a href="#possibility">Portfolio</a></p>
-            <p><a href="#features">Contact</a></p>
-            <p><a href="#blog">Resources</a></p>
+            <Menu />
           </div>
       </div>
+    <div className='dpr__navbar-sign'>
+  <p>Sign in</p>
+  <button type='button'>Sign up</button>
+    </div>
+    <div className='dpr__navbar-menu'>
+{
+  toggleMenu
+  ? <RiCloseLine color='#fff' size={27} onclick ={() => setToggleMenu(false)}/>
+  : <RiMenu3Line color='#fff' size={27} onclick ={() => setToggleMenu(true)}/>
+}
+{toggleMenu && (
+  <div className='dpr__navbar-menu_container scale-up-center'>
+    <div className='dpr__navbar-menu_container-links'> 
+    <Menu />
+    <div className='dpr__navbar-menu_container-links-sign'>
+  <p>Sign in</p>
+  <button type='button'>Sign up</button>
+    </div>
+    </div>
+  </div>
+)
 
+}
+    </div>
     </div>
   )
 }
 
-export default navbar
+export default Navbar
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png'
+import analytic from '../../assets/analytic.png'
+
+
 
 
 const header = () => {
   return (
-    <div>
-      header
-
+    <div dpr__header section__padding>
+    <div className='dpr__header-content'>
+      <h1 className='gradient__text'>
+  Welcome to the analytics alchemy,
+  where data speaks volumes
+      </h1>
+      <p>
+        It takes us and only us to make you realize
+        that the most powerful tool in the digital world is data;
+        your data plus our expertise equals limitless possibilites
+        and unprecedented generation of information and intelligence.
+        An investment you will never regret! 
+      </p>
+      <div className='dpr__header-content__input'>
+        <input type="email" placeholder='Your email address' />
+        <button type='button'>Get Started</button>
+      </div>
+      <div className='dpr__header-content_people'>
+        <img src={people} alt="people" />
+        <p>1600 people requested 24hr access to our resources</p>
+      </div>
+      <div className='dpr__header-image'>
+    <img src={analytic} alt="analytic"/>
+      </div>
     </div>
+</div>
   )
 }
 
