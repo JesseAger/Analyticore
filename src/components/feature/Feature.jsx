fb82ef06b4e0a51258b59a644e3a657d4693000a
JesseAger <agerjesse6@gmail.com>
2024-02-24 09:52:40 +0300

    features_gradient

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -4,7 +4,7 @@ import './feature.css';
 
 const feature = ({title, text}) => {
   return (
-    <div className='dpr__features-containers__feature'>
+    <div className='dpr__features-container__feature'>
     <div className='dpr__features-contaniners__feature-title'>
       <div />
       <h1>{title}</h1>
