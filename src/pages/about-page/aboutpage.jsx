ff68b27fe8413d996f449c443d0ff63173493da3
JesseAger <agerjesse6@gmail.com>
2024-02-25 00:48:37 +0300

    pages_add_onclick

---
+++ src/pages/about-page/aboutpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const aboutpage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default aboutpage
+
---
+++ src/pages/page-exports.js
@@ -0,0 +1,4 @@
+export {default as aboutpage} from './pages/about-page';
+export {default as contactUs} from './contact';
+export {default as portfolio} from './portfolio';
+export {default as resources} from './resources';
